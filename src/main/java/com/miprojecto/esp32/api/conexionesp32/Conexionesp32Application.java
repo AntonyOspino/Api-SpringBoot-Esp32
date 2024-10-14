989db877a91f7fb31d68db37e11f694c28326c39
AntonyOspino <tonny1998_thony@hotmail.com>
2024-10-14 05:50:20 -0500

    modificacion en las variables de Bd

--- src/main/java/com/miprojecto/esp32/api/conexionesp32/Conexionesp32Application.java
+++ src/main/java/com/miprojecto/esp32/api/conexionesp32/Conexionesp32Application.java
@@ -10,9 +10,9 @@ public class Conexionesp32Application {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
-		System.out.println("DB_URL: " + dotenv.get("DB_URL")); // Imprimir variable para verificar
-		System.out.println("DB_USERNAME: " + dotenv.get("DB_USERNAME")); // Imprimir variable para verificar
-		System.out.println("DB_PASSWORD: " + dotenv.get("DB_PASSWORD")); // Imprimir variable para verificar
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(Conexionesp32Application.class, args);
 	}
 
