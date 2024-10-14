8ae879b39f9d273441931a243c17ea16cc091735
AntonyOspino <tonny1998_thony@hotmail.com>
2024-10-14 05:13:25 -0500

    agregando .env , modificacion de dependencias y cambio en clase principal

--- src/main/java/com/miprojecto/esp32/api/conexionesp32/Conexionesp32Application.java
+++ src/main/java/com/miprojecto/esp32/api/conexionesp32/Conexionesp32Application.java
@@ -1,5 +1,6 @@
 package com.miprojecto.esp32.api.conexionesp32;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Conexionesp32Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.out.println("DB_URL: " + dotenv.get("DB_URL")); // Imprimir variable para verificar
+		System.out.println("DB_USERNAME: " + dotenv.get("DB_USERNAME")); // Imprimir variable para verificar
+		System.out.println("DB_PASSWORD: " + dotenv.get("DB_PASSWORD")); // Imprimir variable para verificar
 		SpringApplication.run(Conexionesp32Application.class, args);
 	}
 
